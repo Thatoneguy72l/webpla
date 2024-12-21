4cb3739c2d2672e427a961837114ded679067c4c
Thatoneguy72l <garciatimothy53@mygya.com>
2024-12-20 21:14:34 -0800

    Delete models directory

--- models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-    mediaUrl: String,
-    description: String,
-    comments: [String], // Array of comments
-});
-
-module.exports = mongoose.model('Post', PostSchema);
